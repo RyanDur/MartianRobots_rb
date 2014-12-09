6a341c018751103a5cd2f2503579f6b5f51db2ea
RyanDur <ryan.p.durling@gmail.com>
2014-12-09 13:33:53 +0000

    refactor bound check

--- lib/mars.rb
+++ lib/mars.rb
@@ -5,16 +5,17 @@ class Mars
 
   def initialize
     @boundary = lambda { |max, coord| coord > max || coord < 0 }
+    set_boundaries(@boundary.curry.(MAX_GRID_SIZE), @boundary.curry.(MAX_GRID_SIZE))
   end
 
   def setup(x, y)
-    raise ArgumentError if invalid_boundary?(x) || invalid_boundary?(y)
+    raise ArgumentError if @out_of_bounds.(x, y)
     set_boundaries(@boundary.curry.(x), @boundary.curry.(y))
   end
 
 
   def set_robot(robot)
-    raise ArgumentError if @out_of_bounds.(robot)
+    raise ArgumentError if @out_of_bounds.(robot.location.x, robot.location.y)
     @robot = robot
     @lost = false
   end
@@ -27,17 +28,13 @@ class Mars
     raise ArgumentError if instructions.size >= MAX_INSTRUCTION_SIZE
     instructions.each_char do |instruction|
       @robot.move(instruction)
-      (@lost = true) and break if @out_of_bounds.(@robot)
+      (@lost = true) and break if @out_of_bounds.(@robot.location.x, @robot.location.y)
     end
   end
 
   private
 
   def set_boundaries(boundary_x, boundary_y)
-    @out_of_bounds = lambda { |robot| boundary_x.(robot.location.x) || boundary_y.(robot.location.y) }
-  end
-
-  def invalid_boundary?(coord)
-    @boundary.curry.(MAX_GRID_SIZE).(coord)
+    @out_of_bounds = lambda { |x, y| boundary_x.(x) || boundary_y.(y) }
   end
 end
