b9378975062965a875b919f73933fc8b16c15564
RyanDur <ryan.p.durling@gmail.com>
2014-12-15 23:15:50 +0000

    remove unnecessary functions

--- lib/robot.rb
+++ lib/robot.rb
@@ -3,23 +3,10 @@ class Robot
 
   def initialize(position)
     @position = position
-    @lost = false
-  end
-
-  def set_lost(lost)
-    @lost = lost
-  end
-
-  def lost?
-    @lost
   end
 
   def move(instruction)
     self.class.new(instruction.execute(@position))
-  end
-
-  def ==(other)
-    @position == other.position
   end
 
   def to_s
--- spec/lib/robot_spec.rb
+++ spec/lib/robot_spec.rb
@@ -23,32 +23,4 @@ describe Robot do
     allow(position).to receive(:to_s).and_return('wassup')
     expect("#{robot}").to eq 'wassup'
   end
-
-  context 'equal' do
-    let(:position2) { double('Position') }
-    let(:robot2) { Robot.new(position2) }
-
-    it 'should override ==' do
-      allow(position).to receive(:==).and_return(true)
-      expect(robot == robot2).to be(true)
-    end
-  end
-
-  context 'not equal' do
-    let(:position2) { double('Position') }
-    let(:robot2) { Robot.new(position2) }
-
-    it 'should override ==' do
-      expect(robot == robot2).to be(false)
-    end
-  end
-
-  it 'should be able to see if a list contains a similar position' do
-    s = []
-    allow(instruction).to receive(:execute).with(position).and_return(position)
-    allow(position).to receive(:==).and_return(true)
-    other = robot.move(instruction)
-    s.push(other)
-    expect(s.include?(other)).to be true
-  end
 end
