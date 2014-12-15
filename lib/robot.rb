0c59a1426d39ef3ceca9cab3341f5686c87791bf
RyanDur <ryan.p.durling@gmail.com>
2014-12-15 23:56:46 +0000

    rdoc for robot

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,14 +1,28 @@
 class Robot
   attr_accessor :position
 
+  ##
+  # Creates a robot to traverse the surface of mars
+  #
+  # @param [Position] position
+  # @return [Robot]
   def initialize(position)
     @position = position
   end
 
+  ##
+  # Takes in an instruction and returns a robot at the position based on the instruction
+  #
+  # @param [Instruction] instruction
+  # @return [Robot]
   def move(instruction)
     self.class.new(instruction.execute(@position))
   end
 
+  ##
+  # retruns the position of the robot as a string
+  #
+  # @return [String]
   def to_s
     @position.to_s
   end
