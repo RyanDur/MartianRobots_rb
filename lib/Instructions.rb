1d5093a75eaa5c2df2769050751a9e6107a985b8
RyanDur <ryan.p.durling@gmail.com>
2014-12-15 23:11:00 +0000

    rdoc for instruction list

--- lib/Instructions.rb
+++ lib/Instructions.rb
@@ -5,6 +5,14 @@ require_relative 'lang/messages'
 class Instructions
   include Messages
 
+  ##
+  # Takes a string of instructions (e.g. "FFR") and returns an array of Instructions.
+  # If an Instruction does not exist, it will raise an ArgumentError that specifies the
+  # the name of the non existent instruction.
+  #
+  # @param [String] instructions
+  # @return [Array] Instructions
+  # @raise [ArgumentError] if an instruction does not exist
   def create(instructions)
     instructions.chars.map { |instruction| Object::const_get(instruction).new }
   rescue NameError => e
