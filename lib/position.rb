c42ec69992daa8869e2c65a5c421ba7b1d48992a
RyanDur <ryan.p.durling@gmail.com>
2014-12-16 00:01:19 +0000

    rdoc for a position

--- lib/position.rb
+++ lib/position.rb
@@ -1,20 +1,41 @@
 class Position
   attr_accessor :x, :y, :orientation
-
+  ##
+  # Creates a position
+  #
+  # @param [Integer] x
+  # @param [Integer] y
+  # @param [String] orientation
   def initialize(x, y, orientation)
     @x = x
     @y = y
     @orientation = orientation
   end
 
+  ##
+  # Takes a pair of coordinates and an orientation and returns a new position
+  #
+  # @param [Integer] x
+  # @param [Integer] y
+  # @param [String] orientation
+  # @return [Position]
   def set(x, y, orientation)
     self.class.new(x, y, orientation)
   end
 
+  ##
+  # Checks to see if the other position is at the same xy coordinates
+  #
+  # @param [Position] other
+  # @return [Boolean]
   def ==(other)
     other.x == @x && other.y == @y
   end
 
+  ##
+  # returns the xy and orientation as a string
+  #
+  # @return [String]
   def to_s
     "#{@x} #{@y} #{@orientation}"
   end
