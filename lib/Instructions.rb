b6a1c3a5a2ba1ab9dd68beb7f9cabd73b2df3b2b
RyanDur <ryan.p.durling@gmail.com>
2014-12-16 17:45:55 +0000

    make instructions use swithc case instead of reflection

--- lib/Instructions.rb
+++ lib/Instructions.rb
@@ -1,12 +1,15 @@
-project_root = File.dirname(File.absolute_path(__FILE__))
-Dir.glob(project_root + '/instructions/*', &method(:require))
+require_relative 'instructions/F'
+require_relative 'instructions/L'
+require_relative 'instructions/R'
 require_relative 'lang/messages'
+require_relative 'lang/instruction_list'
 
 class Instructions
+  include InstructionList
   include Messages
 
   ##
-  # Takes a string of instructions (e.g. "FFR") and returns an array of Instructions.
+  # Takes a string of instructions (e.g. "FLR") and returns an array of Instructions.
   # If an Instruction does not exist, it will raise an ArgumentError that specifies the
   # the name of the non existent instruction.
   #
@@ -14,8 +17,23 @@ class Instructions
   # @return [Array] Instructions
   # @raise [ArgumentError] if an instruction does not exist
   def create(instructions)
-    instructions.chars.map { |instruction| Object::const_get(instruction).new }
+    instructions.chars.map { |instruction| get(instruction) }
   rescue NameError => e
     raise ArgumentError, not_exist(e.name)
+  end
+
+  private
+
+  def get(instruction)
+    case instruction
+      when FORWARD
+        return F.new
+      when LEFT
+        return L.new
+      when RIGHT
+        return R.new
+      else
+        raise NameError.new('', instruction)
+    end
   end
 end
---
+++ lib/lang/instruction_list.rb
@@ -0,0 +1,5 @@
+module InstructionList
+  FORWARD = 'F'
+  LEFT = 'L'
+  RIGHT = 'R'
+end
